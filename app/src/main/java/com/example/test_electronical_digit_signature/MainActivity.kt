001ec9bfa63ff697a907f82b9e39012ab8449a2a
milkfist <alyonazzz12@mail.ru>
2023-12-05 18:08:47 +0300

    added saving signature on phone storage (in Downloads)

--- app/src/main/java/com/example/test_electronical_digit_signature/MainActivity.kt
+++ app/src/main/java/com/example/test_electronical_digit_signature/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.example.test_electronical_digit_signature
 
+import android.content.ContentValues
+import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.test_electronical_digit_signature.databinding.ActivityMainBinding
@@ -24,6 +29,8 @@ class MainActivity : AppCompatActivity() {
     private val TAG = "MainActivity1"
 
     private lateinit var binding: ActivityMainBinding
+    private var signturePath = ""
+    private var signtureFileName = "noDocument_signature"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -60,31 +67,6 @@ class MainActivity : AppCompatActivity() {
             val privateKey: PrivateKey = keyPair.private
             val publicKey: PublicKey = keyPair.public
 
-
-////        val filePath = "\\home\\milkfist\\02.txt"
-////        val filePath = "//home//milkfist//02.txt"
-//        val filePath = "/home/milkfist/02.txt"
-////        val filePath = "//home//milkfist//AndroidStudioProjects//Test_Electronical_Digit_Signature//app//ic_launcher_background.xml"
-////        val filePath = "\\home\\milkfist\\AndroidStudioProjects\\Test_Electronical_Digit_Signature\\app\\ic_launcher_background.xml"
-//        val path: Path = Paths.get(filePath)
-//
-//        if (Files.exists(path)) {
-//            if (Files.isReadable(path)) {
-//                println("Файл можно читать.")
-//            } else {
-//                println("У программы нет разрешения на чтение файла.")
-//            }
-//
-//            if (Files.isWritable(path)) {
-//                println("В файл можно писать.")
-//            } else {
-//                println("У программы нет разрешения на запись в файл.")
-//            }
-//        } else {
-//            println("Файл не существует.")
-//        }
-
-
 //            // Создаем подпись
 ////            val signature = createSignature("путь_к_файлу_для_подписи.txt", privateKey)
 //            val signature = createSignature("/home/milkfist/Загрузки/", privateKey)
@@ -92,9 +74,20 @@ class MainActivity : AppCompatActivity() {
 //
             // Сохраняем подпись в файл
 //            Files.write(Paths.get("путь_к_файлу_подписи.txt"), signature)
+//            Files.write(Paths.get("/document/primary:Download"), signature)
+//            File(signturePath).writeBytes(signature)
+            println("signature1 = ${signature.contentToString()}")
+//            File("document/signaturePath.txt").writeBytes(signature)
+            // Укажите путь и имя файла, который вы хотите создать
+            val filePath = "/document/f.txt"
+//            createFile(filePath)
+            createFile(this, signtureFileName)
+
+
 //
             // Проверяем подпись
 //            val isSignatureValid = verifySignature("путь_к_файлу_для_подписи.txt", publicKey, signature)
+//            val isSignatureValid = verifySignature(publicKey, ???"signature.txt")
             val isSignatureValid = verifySignature(publicKey, signature)
             if (isSignatureValid) {
                 println("Подпись верна.")
@@ -121,6 +114,8 @@ class MainActivity : AppCompatActivity() {
             val path: String = uri?.path.toString()
             val file = File(path)
             binding.tvFileInfo.text = "fileName = ${file.name}, path = $path".trimIndent()
+            //
+            signturePath = path.replace(file.name, "sign.txt")
         }
     }
 
@@ -148,5 +143,39 @@ class MainActivity : AppCompatActivity() {
 
             return verifier.verify(signature)
         }
+
+
+//    fun createFile(filePath: String) {
+//        val path: Path = Paths.get(filePath)
+//
+//        try {
+//            Files.createFile(path)
+//            println("Файл успешно создан: $filePath")
+//        } catch (e: Exception) {
+//            println("Ошибка при создании файла: ${e.message}")
+//        }
+//    }
+    private fun createFile(context: Context, fileName: String) {
+        val contentValues = ContentValues().apply {
+            put(MediaStore.MediaColumns.DISPLAY_NAME, fileName)
+            put(MediaStore.MediaColumns.MIME_TYPE, "text/plain")
+//            put(MediaStore.MediaColumns.MIME_TYPE, "sig")
+//            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_DOWNLOADS)
+//            }
+        }
+
+        val resolver = context.contentResolver
+        val uri = resolver.insert(MediaStore.Files.getContentUri("external"), contentValues)
+
+        uri?.let {
+            resolver.openOutputStream(uri)?.use { outputStream ->
+                // Напишите данные в поток
+                outputStream.write("Привет, мир!".toByteArray())
+            }
+
+            println("файл успешно создан: ${uri.path}")
+        }
+    }
 
 }
