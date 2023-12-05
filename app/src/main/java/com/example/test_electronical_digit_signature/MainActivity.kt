b3e8905e7d8d372056022fe452cf44953d9e938f
milkfist <alyonazzz12@mail.ru>
2023-12-06 00:07:06 +0300

    deleted all old comments

--- app/src/main/java/com/example/test_electronical_digit_signature/MainActivity.kt
+++ app/src/main/java/com/example/test_electronical_digit_signature/MainActivity.kt
@@ -4,7 +4,6 @@ import android.content.ContentValues
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
-import android.os.Build
 import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
@@ -20,7 +19,6 @@ import kotlinx.coroutines.launch
 import org.bouncycastle.jce.provider.BouncyCastleProvider
 import java.io.File
 import java.nio.file.Files
-import java.nio.file.Path
 import java.nio.file.Paths
 import java.security.KeyPair
 import java.security.KeyPairGenerator
@@ -50,7 +48,6 @@ class MainActivity : AppCompatActivity() {
 
         binding.btnFilePicker.setOnClickListener {
             CoroutineScope(Dispatchers.Main).launch {
-//                showFileChooser()
                 val firstResult: Deferred<String> = async { showFileChooser() }
                 delay(10_000)
                 // Запускаем вторую функцию только после завершения первой
@@ -70,9 +67,6 @@ class MainActivity : AppCompatActivity() {
         // of that it's possible to have another BC implementation loaded in VM.
         Security.removeProvider(BouncyCastleProvider.PROVIDER_NAME)
         Security.insertProviderAt(BouncyCastleProvider(), 0)
-
-//            // Регистрируем Bouncy Castle в качестве провайдера безопасности
-////            Security.addProvider(BouncyCastleProvider())
 //
             // Создаем генератор ключевой пары с использованием алгоритма, например, RSA
             val keyPairGenerator = KeyPairGenerator.getInstance("RSA", "BC")
@@ -86,27 +80,15 @@ class MainActivity : AppCompatActivity() {
             publicKey= keyPair.public
 
         }
-
+//
     private fun afterShowFileChooser(await: String) {
         // Создаем подпись
-        //            val signature = createSignature("путь_к_файлу_для_подписи.txt", privateKey)
         val signature = createSignature(filePath, privateKey)
-        //
-        // Сохраняем подпись в файл
-        //            Files.write(Paths.get("путь_к_файлу_подписи.txt"), signature)
-        //            Files.write(Paths.get("/document/primary:Download"), signature)
-        //            File(signturePath).writeBytes(signature)
         println("signature1 = ${signature.contentToString()}")
-        //            File("document/signaturePath.txt").writeBytes(signature)
-        // Укажите путь и имя файла, который вы хотите создать
-        //            createFile(filePath)
+        // Сохраняем подпись в файл
         createFile(this@MainActivity, signtureFileName, signature)
 
-
-        //
         // Проверяем подпись
-        //            val isSignatureValid = verifySignature("путь_к_файлу_для_подписи.txt", publicKey, signature)
-        //            val isSignatureValid = verifySignature(publicKey, ???"signature.txt")
         val isSignatureValid = verifySignature(filePath, publicKey, signature)
         if (isSignatureValid) {
             println("Подпись верна.")
@@ -115,8 +97,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    //    private fun showFileChooser() {
-    private suspend fun showFileChooser(): String {
+    private fun showFileChooser(): String {
         val intent = Intent(Intent.ACTION_GET_CONTENT)
         intent.type = "*/*"
         intent.addCategory(Intent.CATEGORY_OPENABLE)
@@ -136,22 +117,14 @@ class MainActivity : AppCompatActivity() {
             filePath = path
             val file = File(path)
             binding.tvFileInfo.text = "fileName = ${file.name}, path = $path".trimIndent()
-            //
-            signturePath = path.replace(file.name, "sign.txt")
         }
     }
 
     //
         private fun createSignature(filePath: String, privateKey: PrivateKey): ByteArray {
-//        private fun createSignature(privateKey: PrivateKey): ByteArray {
             val signature = Signature.getInstance("SHA256withRSA", "BC")
             signature.initSign(privateKey)
-
-//            val fileBytes = Files.readAllBytes(Paths.get(filePath))
-//            val fileBytes = "Paths.get(filePath)".toByteArray()
             try {
-//                val fileHash = getFileHash(filePath)
-//                val hashByteArray = hexStringToByteArray(fileHash)
                 val hashByteArray = File(filePath).hashCode().toString().toByteArray()
 
                 // Теперь у вас есть массив байтов, представляющих хэш файла
@@ -162,35 +135,20 @@ class MainActivity : AppCompatActivity() {
                 println("Ошибка  кодирования хэш-кода: ${e.message}")
                 Toast.makeText(this, "Ошибка кодирования хэш-кода", Toast.LENGTH_SHORT).show()
             }
-//            signature.update(hashByteArray)
 
             return signature.sign()
         }
 //
         fun verifySignature(filePath: String, publicKey: PublicKey, signature: ByteArray): Boolean {
-//        fun verifySignature(publicKey: PublicKey, signature: ByteArray): Boolean {
             val verifier = Signature.getInstance("SHA256withRSA", "BC")
             verifier.initVerify(publicKey)
 
-//            val fileBytes = Files.readAllBytes(Paths.get(filePath))
-//            val fileBytes = "Paths.get(filePath)".toByteArray()
             val fileBytes = File(filePath).hashCode().toString().toByteArray()
             verifier.update(fileBytes)
 
             return verifier.verify(signature)
         }
 
-
-//    fun createFile(filePath: String) {
-//        val path: Path = Paths.get(filePath)
-//
-//        try {
-//            Files.createFile(path)
-//            println("Файл успешно создан: $filePath")
-//        } catch (e: Exception) {
-//            println("Ошибка при создании файла: ${e.message}")
-//        }
-//    }
     private fun createFile(context: Context, fileName: String, signature: ByteArray) {
         val contentValues = ContentValues().apply {
             put(MediaStore.MediaColumns.DISPLAY_NAME, fileName)
@@ -214,27 +172,4 @@ class MainActivity : AppCompatActivity() {
             println("файл успешно создан: ${uri.path}")
         }
     }
-
-
-    //
-    fun getFileHash(filePath: String): String {
-        val fileBytes = Files.readAllBytes(Paths.get(filePath))
-        val messageDigest = MessageDigest.getInstance("SHA-256")
-        val digest = messageDigest.digest(fileBytes)
-
-        // Преобразование массива байтов хэша в шестнадцатеричное представление
-        return digest.joinToString("") { "%02x".format(it) }
-    }
-
-    fun hexStringToByteArray(hexString: String): ByteArray {
-        val result = ByteArray(hexString.length / 2)
-
-        for (i in 0 until hexString.length step 2) {
-            val byte = hexString.substring(i, i + 2).toInt(16).toByte()
-            result[i / 2] = byte
-        }
-
-        return result
-    }
-
 }
