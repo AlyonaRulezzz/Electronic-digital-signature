c8aab4f4afc239d3bcb623e3aa301f7a52da0d9a
milkfist <alyonazzz12@mail.ru>
2023-12-06 21:49:07 +0300

    after deleting all comments, generateKeys() is uncomment

--- app/src/main/java/com/example/test_electronical_digit_signature/MainActivity.kt
+++ app/src/main/java/com/example/test_electronical_digit_signature/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.test_electronical_digit_signature
 
 import android.Manifest.permission.READ_EXTERNAL_STORAGE
+import android.annotation.SuppressLint
 import android.content.ContentValues
 import android.content.Context
 import android.content.Intent
+import android.content.pm.ActivityInfo
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Build
@@ -22,6 +24,8 @@ import org.bouncycastle.util.io.pem.PemReader
 import java.io.File
 import java.io.FileReader
 import java.security.KeyFactory
+import java.security.KeyPair
+import java.security.KeyPairGenerator
 import java.security.PrivateKey
 import java.security.PublicKey
 import java.security.Security
@@ -36,7 +40,6 @@ class MainActivity : AppCompatActivity() {
 
     private val PERMISSION_REQUEST_READ_EXTERNAL_STORAGE = 1
 
-    private val TAG = "MainActivity1"
     private val PASSWORD = "1"
 
     private lateinit var binding: ActivityMainBinding
@@ -51,6 +54,17 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+
+        val provider = Security.getProvider(BouncyCastleProvider.PROVIDER_NAME)
+        Security.removeProvider(provider.name)
+        Security.insertProviderAt(BouncyCastleProvider(), 1)
+
+        if (checkSelfPermission(READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(
+                arrayOf(READ_EXTERNAL_STORAGE),
+                PERMISSION_REQUEST_READ_EXTERNAL_STORAGE)
+        }
 
         binding.btnFilePicker.setOnClickListener {
             initViews()
@@ -58,40 +72,30 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.btnSign.setOnClickListener {
-            if (privateKey != null && publicKey != null) {
-                if ((filePath != "") && (binding.etPassword.text.toString() == PASSWORD)) {
-                    isSigningError = false
-                    afterShowFileChooser()
-                } else {
-                    isSigningError = true
-                }
-
-                if (isSigningError) {
-                    binding.tvSignStatus.text = "Ошибка подписания документа"
-                } else {
-                    binding.tvSignStatus.text = "Подписание документа завершено успешно"
-                }
-            }
+            signCheck()
         }
 
-        binding.etPassword.doOnTextChanged { text, start, before, count ->
+        binding.etPassword.doOnTextChanged { text, _, _, _ ->
             isSigningError = text != PASSWORD
         }
+    }
 
-//
-        val provider = Security.getProvider(BouncyCastleProvider.PROVIDER_NAME)
-        Security.removeProvider(provider.name)
-        Security.insertProviderAt(BouncyCastleProvider(), 1)
+    private fun signCheck() {
+        if (privateKey != null && publicKey != null) {
+            if ((filePath != "") && (binding.etPassword.text.toString() == PASSWORD)) {
+                isSigningError = false
+                afterShowFileChooser()
+            } else {
+                isSigningError = true
+            }
 
-        if (checkSelfPermission(READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            println(
-                "checkSelfPermission(READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED = " +
-                        "${checkSelfPermission(READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED}"
-            )
-            // Запрашиваем разрешение у пользователя
-            requestPermissions(
-                arrayOf(READ_EXTERNAL_STORAGE),
-                PERMISSION_REQUEST_READ_EXTERNAL_STORAGE)
+            if (isSigningError) {
+                binding.tvSignStatus.text = getString(R.string.signing_error)
+            } else {
+                binding.tvSignStatus.text = getString(R.string.success_signing)
+            }
+        } else {
+            binding.tvSignStatus.text = getString(R.string.signing_error)
         }
     }
 
@@ -105,12 +109,9 @@ class MainActivity : AppCompatActivity() {
 
     //
     private fun afterShowFileChooser() {
-        // Создаем подпись
         privateKey?.let{
             publicKey?.let {
                 val signature = createSignature(filePath, privateKey!!)
-                println("signature1 = ${signature.contentToString()}")
-                // Сохраняем подпись в файл
                 createFile(this@MainActivity, signtureFileName, signature)
             }
         }
@@ -120,8 +121,9 @@ class MainActivity : AppCompatActivity() {
         binding.tvPublicKeyStatus.text = ""
         binding.tvPrivateKeyStatus.text = ""
 
-        searchPrivateKey()
-        searchPublicKey()
+//        searchPrivateKey()
+//        searchPublicKey()
+        generateKeys()
 
         val intent = Intent(Intent.ACTION_GET_CONTENT)
         intent.type = "*/*"
@@ -134,57 +136,52 @@ class MainActivity : AppCompatActivity() {
         return filePath
     }
 
+    private fun generateKeys() {
+        val keyPairGenerator = KeyPairGenerator.getInstance("RSA", "BC")
+        keyPairGenerator.initialize(2048)
+        val keyPair: KeyPair = keyPairGenerator.generateKeyPair()
+        privateKey = keyPair.private
+        publicKey = keyPair.public
+    }
+
     private fun searchPrivateKey(){
-        // Поиск приватного ключа
         val directoryPath = Environment.getExternalStorageDirectory().path
         val directory = File(directoryPath)
         val extensionToFind = "pem"
         val pemFiles = findFilesByExtension(directory, extensionToFind)
         if (pemFiles.isNotEmpty()) {
-            println("Найденные файлы с расширением $extensionToFind:")
             for (pemFile in pemFiles) {
                 println(pemFile.absolutePath)
             }
             val pemFilePath = pemFiles[0].absolutePath
             privateKey = getPrivateKeyFromPEMFile(pemFilePath)
-            println("Закрытый ключ: $privateKey")
         } else {
-            println("Файлы с расширением $extensionToFind не найдены.")
-            println("Не удалось извлечь закрытый ключ.")
             binding.tvPrivateKeyStatus.text = this.getString(R.string.no_pivate_key)
         }
     }
 
     private fun searchPublicKey() {
-        // Путь к корневой директории, где вы хотите начать поиск публичного ключа
         val directoryPath = Environment.getExternalStorageDirectory().path
         val extensionToFind = "cer"
         val directory = File(directoryPath)
         val cerFiles = findFilesByExtension(directory, extensionToFind)
         if (cerFiles.isNotEmpty()) {
-            println("Найденные файлы с расширением $extensionToFind:")
             for (cerFile in cerFiles) {
                 println(cerFile.absolutePath)
             }
             val certificateFilePath = cerFiles[0].absolutePath
             publicKey = getPublicKeyFromCertificateFile(certificateFilePath)
-            // Проверяем ключи
             val isSignatureValid = publicKey?.let { verifyKeys("Hello world!", it) }
-            if (isSignatureValid == true) {
-                println("Подпись верна.")
-                println("Открытый ключ: $publicKey")
-            } else {
-                println("Подпись не верна.")
-                println("Не удалось извлечь открытый ключ.")
+            if (isSignatureValid == false) {
                 binding.tvPublicKeyStatus.text = this.getText(R.string.no_public_key)
             }
         } else {
-            println("Файлы с расширением $extensionToFind не найдены.")
-            println("Не удалось извлечь открытый ключ.")
             binding.tvPublicKeyStatus.text = this.getText(R.string.no_public_key)
         }
     }
 
+    @Deprecated("Deprecated in Java")
+    @SuppressLint("SetTextI18n")
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == 100 && resultCode == RESULT_OK && data != null) {
@@ -202,17 +199,11 @@ class MainActivity : AppCompatActivity() {
             signature.initSign(privateKey)
             try {
                 val hashByteArray = File(filePath).hashCode().toString().toByteArray()
-
-                // Теперь у вас есть массив байтов, представляющих хэш файла
-                println("Хэш файла в виде массива байтов: ${hashByteArray.contentToString()}")
-//                println("Хэш файла в виде массива байтов...")
                 signature.update(hashByteArray)
             } catch (e: Exception) {
-                println("Ошибка  кодирования хэш-кода: ${e.message}")
-                Toast.makeText(this, "Ошибка кодирования хэш-кода", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, getString(R.string.hashcode_error), Toast.LENGTH_SHORT).show()
                 isSigningError = true
             }
-
             return signature.sign()
         }
 //
@@ -244,11 +235,8 @@ class MainActivity : AppCompatActivity() {
 
     private fun createFile(context: Context, fileName: String, signature: ByteArray) {
         val contentValues = ContentValues().apply {
-//            put(MediaStore.MediaColumns.MIME_TYPE, "text/plain")
             put(MediaStore.MediaColumns.DISPLAY_NAME, "${signtureFileName.substringBeforeLast('.')}.sig")
-//            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
                 put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_DOWNLOADS)
-//            }
         }
 
         val resolver = context.contentResolver
@@ -256,46 +244,38 @@ class MainActivity : AppCompatActivity() {
 
         uri?.let {
             resolver.openOutputStream(uri)?.use { outputStream ->
-                // Напишите данные в поток
-//                outputStream.write("Привет, мир!".toByteArray())
                 outputStream.write(signature)
             }
-
-            println("файл успешно создан: ${uri.path}")
         }
     }
-    //
+
     private fun getPublicKeyFromCertificateFile(certificateFilePath: String): PublicKey? {
-        try {
+        return try {
             val pemReader = PemReader(FileReader(certificateFilePath))
             val certHolder = pemReader.readPemObject() as X509CertificateObject
 
-            // Convert X509CertificateHolder to X509Certificate
             val certFactory = CertificateFactory.getInstance("X.509")
             val x509Certificate = certFactory.generateCertificate(certHolder.encoded.inputStream()) as X509Certificate
 
-            // Extract public key from X509Certificate
-            return x509Certificate.publicKey
+            x509Certificate.publicKey
         } catch (e: Exception) {
             e.printStackTrace()
-            return null
+            null
         }
     }
 
-    fun getPrivateKeyFromPEMFile(pemFilePath: String): PrivateKey? {
-        try {
+    private fun getPrivateKeyFromPEMFile(pemFilePath: String): PrivateKey? {
+        return try {
             val pemReader = PemReader(FileReader(pemFilePath))
             val pemObject = pemReader.readPemObject()
 
-            // Convert PEM object to PrivateKey
             val privateKeyBytes = pemObject.content
-            // Create a PrivateKey object using the decoded private key
             val keyFactory: KeyFactory = KeyFactory.getInstance("RSA", BouncyCastleProvider())
             val privateKeySpec: EncodedKeySpec = X509EncodedKeySpec(privateKeyBytes)
-            return keyFactory.generatePrivate(privateKeySpec)
+            keyFactory.generatePrivate(privateKeySpec)
         } catch (e: Exception) {
             e.printStackTrace()
-            return null
+            null
         }
     }
 
@@ -307,18 +287,14 @@ class MainActivity : AppCompatActivity() {
             if (files != null) {
                 for (file in files) {
                     if (file.isDirectory) {
-                        // Рекурсивно вызываем функцию для поддиректории
                         result.addAll(findFilesByExtension(file, extension))
 //                        result.addAll(findFilesByExtension(file, file.name.substringAfterLast('.')))
-//                        println(file.name)
                     } else {
                         // Проверяем расширение файла
-//                        if (file.extension.equals(extension, ignoreCase = true)) {
+                        if (file.extension.equals(extension, ignoreCase = true)) {
 //                        if (file.name.substringAfterLast('.').equals(extension, ignoreCase = true)) {
-//                        if (file.name.contains(".txt", ignoreCase = true)) {
                             result.add(file)
-//                            println(file.name)
-//                        }
+                        }
                     }
                 }
             }
@@ -330,11 +306,8 @@ class MainActivity : AppCompatActivity() {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         when (requestCode) {
             PERMISSION_REQUEST_READ_EXTERNAL_STORAGE -> {
-                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    println("Разрешение получено")
-                } else {
-                    println("Разрешение не получено")
-                    binding.tvSignStatus.text = "Для продолжения работы необходим доступ к файлам на устройстве."
+                if (!(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    binding.tvSignStatus.text = getString(R.string.no_access_to_storage)
                 }
             }
         }
